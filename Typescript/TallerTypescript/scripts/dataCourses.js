ad698b5378cdf05c0d23a214950232f646223612
Juan Manuel Rivera Lopez <jm.rivera@uniandes.edu.co>
2021-04-15 00:48:57 -0500

    Taller 1 TypeScript

---
+++ Typescript/TallerTypescript/scripts/dataCourses.js
@@ -0,0 +1,9 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Ingeniería de Sw", "Jose Bocanegra", 4),
+    new Course("TI en las organizaciones", "Disney Rubiano", 3),
+    new Course("DALGO", "Jorge Duitama", 3),
+    new Course("Probabilidad y estadística I", "Daniela Angulo", 3),
+    new Course("Sistrans", "German Bravo", 6),
+    new Course("Biología celular teórica", "Zyra Garavito", 3)
+];
